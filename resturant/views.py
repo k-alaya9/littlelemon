1ea968509b27fece6a1a3c73536d7e9ba87003a1
k-alaya9 <mayalove314@gmail.com>
2025-09-21 19:35:33 +0300

    `Added rest_framework and resturant app to settings.py and updated resturant app with new views and urls.`

--- resturant/views.py
+++ resturant/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Menu
+from .serilaizers import menuSerializer
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 
 # Create your views here.
 def index(requrest):
     return render(requrest,'index.html',{})
+
+
+class MenuItemsView(ListCreateAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=menuSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    queryset=Menu.objects.all()
+    serializer_class=menuSerializer
+    lookup_field='pk'
